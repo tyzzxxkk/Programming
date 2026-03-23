31bd8e4234de6d155f61beefd2c330c7066f129c
태연 <s2401@e-mirim.hs.kr>
2026-03-23 14:37:48 +0900

    counter HTML

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ let count = 0;
 
 const resultH1 = document.getElementsByTagName('h1')[0];
 const plusButton = document.getElementsByTagName('button')[0];
+const minusButton = document.getElementsByTagName('button')[1];
 
 plusButton.addEventListener("click", () => {
     count++;
+    resultH1.innerText = count;
+});
+
+minusButton.addEventListener("click", () => {
+    count--;
     resultH1.innerText = count;
 });
