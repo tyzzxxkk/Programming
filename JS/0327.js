3bcff37b5019dc980085d70f7b4d41bdff51823e
태연 <s2401@e-mirim.hs.kr>
2026-03-30 13:08:50 +0900

    JS

---
+++ JS/0327.js
@@ -0,0 +1,13 @@
+// const students = [
+//     { name: "Kim", age: 21 },
+//     { name: "Lee", age: 18 },
+//     { name: "Park", age: 23 }
+//   ];
+
+// const result = [...students].filter(students => students.age >= 20);
+// console.log(result);
+
+const numbers = [5, 12, 18, 3, 7];
+
+const result = [...numbers].filter(numbers => numbers >= 10);
+console.log(result);
