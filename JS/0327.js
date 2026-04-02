973bab08605add29dcbd8f30249d68e0ea62725b
태연 <s2401@e-mirim.hs.kr>
2026-04-03 08:11:43 +0900

    reduce

--- JS/0327.js
+++ JS/0327.js
@@ -7,7 +7,19 @@
 // const result = [...students].filter(students => students.age >= 20);
 // console.log(result);
 
-const numbers = [5, 12, 18, 3, 7];
+// const numbers = [5, 12, 18, 3, 7];
 
-const result = [...numbers].filter(numbers => numbers >= 10);
-console.log(result);
+// const result = [...numbers].filter(numbers => numbers >= 10);
+// console.log(result);
+
+// 3. 전체 가격 합 구하기 (reduce)
+
+const products = [
+    { name: "pen", price: 1000, category: "stationery" },
+    { name: "notebook", price: 3000, category: "stationery" },
+    { name: "mouse", price: 20000, category: "device" },
+    { name: "keyboard", price: 30000, category: "device" }
+  ];
+
+const result = products.reduce((acc, curr) => acc + curr.price, 0);
+console.log(`3. 전체 가격 합: ${result}원`);
